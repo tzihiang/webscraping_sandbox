0ad35573fb38ed4a8d55944e8f8801c0e89f061d
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-12-17 18:34:53 +0800

    Added timer to keep running

--- main.py
+++ main.py
@@ -13,18 +13,27 @@ def get_card_list(html_text):
     return card_list
 
 
-# Takes in an array of card lists and returns only the names of the cards
+# Takes in an array of card lists and prints only the names of the cards
 def get_names(card_list):
     card_name = card_list.find_all('div', class_="card_name")
     for cards in card_name:
         print(cards.text)
 
 
+# Takes in name of series (booster, SD etc) and card_List which is retrieved from get_card_list method
+def write_to_file(series_name, card_list):
+    card_name = card_list.find_all('div', class_="card_name")
+    for cards in card_name:
+        with open(f'{series_name}.txt', 'w') as f:
+            f.write(cards.text)
+
+
 # Main method to run continuously
 if __name__ == "__main__":
     while True:
         card_list = get_card_list(html_text)
-        get_names(card_list)
-        time_in_minutes = 10
-        printf(f'Waiting {time_in_minutes} minutes...')
-        time.sleep(60*time_in_minutes)
+        # get_names(card_list)
+        write_to_file("BT_04", card_list)
+        # time_in_minutes = 10
+        # print(f'Waiting {time_in_minutes} minutes...')
+        # time.sleep(60*time_in_minutes)
